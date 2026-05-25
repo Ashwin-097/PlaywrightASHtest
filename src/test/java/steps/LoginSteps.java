67fb2139e7be20e05a521bbc5737333b48966fcf
Ashwin <superashwin@gmail.com>
2026-05-25 13:20:02 +0530

    test

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,48 @@
+package steps;
+
+import com.microsoft.playwright.*;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class LoginSteps {
+    Playwright playwright; // Variable playwright type of Playwright object
+    Browser browser; // Represents a specific browser instance (e.g.Chromium)
+    Page page; // Represents a single tab or page within the browser.
+
+    @Before //Hook - runs before each scenario
+    public void setUp() {
+        playwright = Playwright.create(); //Initializes Playwright engine
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); //Launches a visible Chromium Browser
+        page = browser.newPage(); //opens a new Browser Tab
+    }
+
+    @Given("I open the login page")
+    public void openLoginPage() {
+        page.navigate("https://www.saucedemo.com/"); //Navigates to Log In page
+    }
+
+    @When("I enter username {string} and password {string}")
+    public void enterCredentials(String username, String password) {
+        page.fill("#user-name", username); //Fills in username
+        page.fill("#password", password); //Fills password
+    }
+
+    @When("I click the login button")
+    public void clickLogin() {
+        page.click("#login-button"); //Clicks login button
+    }
+
+    @Then("I should see the products page")
+    public void verifyProductsPage() {
+        assertTrue(page.isVisible(".inventory_list")); //Checks of inventory list is visible
+    }
+
+    @After
+    public void tearDown() {
+        browser.close(); //closes browser page
+        playwright.close(); // shuts down playwright engine
+    }
+}
